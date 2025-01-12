d4f16e74db949108fcacaf2dc35dcf8fd9cd31ac
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-01-12 18:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function isSameType(value1, value2) {
   //your js code here
+	if(Number.isNaN(value1)||Number.isNaN(value2)){
+	    return false;
+	}
 	if(typeof(value1)===typeof(value2)){
 		return true;
-	}else if(Number.isNaN(value1)||Number.isNaN(value2)){
-	    return false;
 	}
 	else{
 	    return false;
